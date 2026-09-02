bdcd368979a112a145f1ff0ff9f45cc0a90a724f
codebyAnwesha <anwesha1.mondal@stu.adamasuniversity.ac.in>
2026-09-02 20:39:25 +0530

    Create program4.c

---
+++ program4.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    int num;
+    printf("Enter number of rows:");
+    scanf("%d", &num);
+    for(int i=num; i>=1; i--){
+        for(int j=1; j<=i; j++){
+            printf("*");
+        }
+        printf("\n");
+    }
+    return 0;
+}
