800359bc98e26af5c9da6d38f0a54b080af27c60
codebyAnwesha <anwesha1.mondal@stu.adamasuniversity.ac.in>
2026-09-04 12:26:32 +0530

    Update program18.c

--- program18.c
+++ program18.c
@@ -1,30 +1,22 @@
 #include<stdio.h>
-void bubbleSort(int array[],int size);
+void bubbleSort(int array[], int size);
 int main(){
-  int data[]={6,4,7,8,1,9,5};
-  int size=sizeof(data)/sizeof(data[0]);
-  printf("Original Array:");
-  for(int i=0; i<size; i++) printf("%d",data[i]);
-  printf("\n\n---Internal Steps(Passes)---\n");
-  bubbleSort(data,size);
-  printf("\nFinal Sorted Array:");
-  for(int i=0; i<size; i++) printf("%d",data[i]);
-  printf("\n");
-  return 0;
+  int data[] = {6,4,7,8,1,9,5};
+  int size = sizeof(data)/sizeof(data[0]);
+  bubbleSort(data, size);
+  printf("Sorted Array:\n");
+  for(int i=0; i<size; i++){
+    printf("%d", data[i]);
+  }
 }
-void bubbleSort(int array[],int size){
-  for(int step=0; step<size-1; ++step){
-    for(int i=0; i<size-step-1;++i){
-      if(array[i]>array[i+1]){
-        int temp=array[i];
-        array[i]=array[i+1];
-        array[i+1]=temp;
+void bubbleSort(int array[], int size){
+  for(int step=0; step < size-1; step++){
+    for(int i=0; i < size-step-1; i++){
+      if(array[i] > array[i+1]){
+        int temp = array[i];
+        array[i] = array[i+1];
+        array[i+1] = temp;
       }
     }
-    printf("Pass%d:",step+1);
-    for(int k=0; k<size; k++){
-      printf("%d",array[k]);
-    }
-    printf("\n");
   }
 }
